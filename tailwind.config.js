6d6b20d5948427eb1bfb91050a331772ca287a47
Davi Felipe Silva Clemente <cldavifsc03@gmail.com>
2026-05-29 08:17:48 -0300

    Arquivo de configuração da biblioteca tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        warmbg: "#fbfaf8",
+        panel: "#ffffff",
+        accent: "#d97706",
+        accentdark: "#b45309",
+        textmain: "#292524",
+        textmuted: "#57534e",
+      },
+    },
+  },
+};
